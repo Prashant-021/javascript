c2499d3fca7d5ce9f49519b33088c64136048eb2
Prashant S Patel <pspatel021@gmail.com>
2023-01-31 15:43:52 +0530

    Update variable.js

--- variable.js
+++ variable.js
@@ -1,1 +1,9 @@
-
+var variable_using_var = true;
+let variable_using_let = "let variable";
+const const_number = 123;
+console.log(variable_using_var)
+document.getElementById('demo').innerHTML += variable_using_var
+console.log(variable_using_let)
+document.getElementById('demo').innerHTML += variable_using_let
+console.log(const_number)
+document.getElementById('demo').innerHTML += const_number
