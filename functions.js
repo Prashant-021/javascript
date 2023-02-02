cf2b1baf1def0c364ddea07b6536eb4c0bba955b
Prashant <prashant.p@simformsolutions.com>
2023-02-02 12:48:21 +0530

    Functions update

--- functions.js
+++ functions.js
@@ -88,7 +88,7 @@ console.log(first_class_var(function (){
 const arr_fun = (a, b) => a / b;
 console.log(arr_fun(23, 5))
 
-   
+  
 // closure is the function along with it's lexical environment
 
 //here we can access the grandparent variable inside inner most function
@@ -121,55 +121,6 @@ function grandparent() {
 grandparent();
 
 
-//Timeout + closure
 
-// using var due to global scope the value of i will be refered after loop is traversed 
-// function timeout_plus_closure() {
-//     for (var i = 1; i < 5; i++) {
-//         setTimeout(() => {
-//             console.log(i);
-//         }, i * 1000);
-//     }
-//     console.log("Hello") // This line will be executed first
-// }
-// timeout_plus_closure();
-//output will be 
-// 5
-// 5
-// 5
-// 5
-
-
-// to print number in sequence then change var to let 
-// because of block scope of let the value assign to each timeout callback function will be different
-// function timeout_plus_closure() {
-//     for (let i = 1; i < 5; i++) {
-//         setTimeout(() => {
-//             console.log(i);
-//         }, i * 1000);
-//     }
-//     console.log("Hello") // This line will be executed first
-// }
-// timeout_plus_closure();
-//output will be 
-// 1
-// 2
-// 3
-// 4
-
-
-// to get above output closure cabe used 
-// function timeout_plus_closure() {
-//     for (let i = 1; i < 5; i++) {
-//         function close(i) {
-//             setTimeout(() => {
-//                 console.log(i);
-//             }, i * 1000);
-//         }
-//         close(i);
-//     }
-//     console.log("Hello") // This line will be executed first
-// }
-// timeout_plus_closure();
 
 
